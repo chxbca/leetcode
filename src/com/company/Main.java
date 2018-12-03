a0839b741a2177675121b56e69a1579c049dd75c
chxbca <272141735@qq.com>
2018-12-04 00:16:59 +0800

    输了输了……时间复杂度竟然还不是最低的

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.Arrays;
+
+/**
+ * @author chxbca
+ */
 public class Main {
     public static void main(String[] args) {
         String s = "a-bC-dEf=ghIj";
         Solution solution = new Solution();
-        System.out.println(solution.reverseOnlyLetters(s));
+        int[] arrays = solution.twoSum(new int[]{2, 5, 5, 11}, 10);
+        System.out.println(Arrays.toString(arrays));
     }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,12 +1,17 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class Solution {
     /**
      * 输入："Test1ng-Leet=code-Q!"
      * 输出："Qedo1ct-eeLg=ntse-T!"
      */
-    public String reverseOnlyLetters(String S) {
-        char[] split = S.toCharArray();
+    public String reverseOnlyLetters(String s) {
+        char[] split = s.toCharArray();
         int i = 0, j = split.length - 1;
         while (i <= j) {
             if (isLetter(split[i])) {
@@ -28,5 +33,26 @@ class Solution {
 
     private boolean isLetter(char s) {
         return (s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z');
+    }
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的 两个 整数。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = IntStream.range(0, nums.length).parallel().boxed().
+                collect(Collectors.toMap(i -> nums[i], i -> i, (a, b) -> b, () -> new HashMap<>(nums.length)));
+        for (int i = 0; i < nums.length; i++) {
+            Integer j = map.get(target - nums[i]);
+            if (j != null && i != j) {
+                return new int[]{i, j};
+            }
+        }
+        return null;
     }
 }
