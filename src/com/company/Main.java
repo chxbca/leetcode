6628b506bee49256b407d5e478fd5b1379aeb59b
chxbca <272141735@qq.com>
2019-08-06 16:56:28 +0800

    哎，大佬还是强啊，不看大佬的思路，我就真的写了O(n^2)复杂度的出来了

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,16 @@
 package com.company;
 
-import java.util.Arrays;
-
 /**
  * @author chxbca
  */
 public class Main {
     public static void main(String[] args) {
-        String s = "a-bC-dEf=ghIj";
         Solution solution = new Solution();
-        int[] arrays = solution.twoSum(new int[]{2, 5, 5, 11}, 10);
-        System.out.println(Arrays.toString(arrays));
+        System.out.println(solution.threeSum(new int[]{-2, 0, 1, 1, 2}));
+        System.out.println(solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        System.out.println(solution.threeSum(new int[]{0, 0, 0, 0}));
+        System.out.println(solution.threeSum(new int[]{1, -1, -1, 0}));
+        System.out.println(solution.threeSum(new int[]{-2, 0, 0, 2, 2}));
+        System.out.println(solution.threeSum(new int[]{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0}));
     }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,7 +1,6 @@
 package com.company;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     /**
@@ -92,4 +91,58 @@ class Solution {
         return sum;
     }
 
+    /**
+     * https://leetcode-cn.com/problems/3sum/
+     * <p>
+     * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+     * <p>
+     * 注意：答案中不可以包含重复的三元组。
+     * <p>
+     * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]
+     * <p>
+     * 满足要求的三元组集合为：
+     * [
+     * [-1, 0, 1],
+     * [-1, -1, 2]
+     * ]
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        int length = nums.length;
+        Arrays.sort(nums);
+        int arrSum = 3;
+        if (length < arrSum) {
+            return answer;
+        }
+        for (int i = 0; i < length; i++) {
+            if (nums[i] > 0) {
+                return answer;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = length - 1;
+            while (right > left) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (sum == 0) {
+                    answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left + 1 < length && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (right - 1 > i && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+
+        }
+        return answer;
+    }
 }
