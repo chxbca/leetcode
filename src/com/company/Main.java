83bc9cda803d68b6738343070aa8d1709b78b4ab
chxbca <272141735@qq.com>
2019-08-08 00:28:23 +0800

    两数相加完成

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.threeSum(new int[]{-2, 0, 1, 1, 2}));
-        System.out.println(solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
-        System.out.println(solution.threeSum(new int[]{0, 0, 0, 0}));
-        System.out.println(solution.threeSum(new int[]{1, -1, -1, 0}));
-        System.out.println(solution.threeSum(new int[]{-2, 0, 0, 2, 2}));
-        System.out.println(solution.threeSum(new int[]{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0}));
+        Solution.ListNode listNode_1 = solution.intToListNode(243);
+        Solution.ListNode listNode_2 = solution.intToListNode(564);
+        Solution.ListNode listNode = solution.addTwoNumbers(listNode_1, listNode_2);
     }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -32,6 +32,93 @@ class Solution {
         return (s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z');
     }
 
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+     * <p>
+     * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+     * <p>
+     * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * 输出：7 -> 0 -> 8
+     * 原因：342 + 465 = 807
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/add-two-numbers
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode middlePoint = head;
+        while (l1 != null || l2 != null) {
+            middlePoint = addTwoNumbers(l1, l2, middlePoint);
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return head;
+    }
+
+    private ListNode addTwoNumbers(ListNode l1, ListNode l2, ListNode middlePoint) {
+
+        if (l1 == null) {
+            middlePoint.val += l2.val;
+        } else if (l2 == null) {
+            middlePoint.val += l1.val;
+        } else {
+            middlePoint.val += l1.val + l2.val;
+        }
+
+        if (middlePoint.val / 10 == 1) {
+            middlePoint.val = middlePoint.val % 10;
+            middlePoint.next = new ListNode(1);
+        } else {
+            boolean l1Flag = l1 != null && l1.next != null;
+            boolean l2Flag = l2 != null && l2.next != null;
+            if (l1Flag || l2Flag) {
+                middlePoint.next = new ListNode(0);
+            }
+        }
+        return middlePoint.next;
+    }
+
+    /**
+     * 测试用代码
+     *
+     * @param num
+     * @return
+     */
+    public ListNode intToListNode(int num) {
+        char[] numChars = String.valueOf(num).toCharArray();
+        ListNode head = new ListNode(numChars[numChars.length - 1] - '0');
+        ListNode middlePoint = head;
+        for (int i = numChars.length - 2; i >= 0; i--) {
+            middlePoint.next = new ListNode(numChars[i] - '0');
+            middlePoint = middlePoint.next;
+        }
+        return head;
+    }
+
+
     /**
      * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的 两个 整数。
      * <p>
