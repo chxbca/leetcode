3e0fc3454a9633d02a70ba102d8417dc598469c5
chxbca <272141735@qq.com>
2019-09-29 06:10:08 +0800

    平滑灰度狗屎题，真无聊

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,21 @@
 package com.company;
 
+import java.util.Arrays;
+
 /**
  * @author chxbca
  */
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        Object ans = solution.minCostClimbingStairs(new int[]{
-                0, 0, 1, 1
-        });
-        System.out.println(ans);
+        int[][] ans = solution.imageSmoother(new int[][]{
+                        {2, 3, 4},
+                        {5, 6, 7},
+                        {8, 9, 10},
+                        {11, 12, 13},
+                        {14, 15, 16}
+                }
+        );
+        System.out.println(Arrays.deepToString(ans));
     }
 }
