29911be3a415ae718581579febbd8f01c667f825
chxbca <272141735@qq.com>
2019-09-30 03:10:46 +0800

    暴力解，睡一觉起来，应该能改成O(n)的

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,13 @@
 package com.company;
 
-import java.util.Arrays;
-
 /**
  * @author chxbca
  */
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] ans = solution.imageSmoother(new int[][]{
-                        {2, 3, 4},
-                        {5, 6, 7},
-                        {8, 9, 10},
-                        {11, 12, 13},
-                        {14, 15, 16}
-                }
-        );
-        System.out.println(Arrays.deepToString(ans));
+        int[] a = {4, 3, 2, 6};
+        int ans = solution.maxRotateFunction(a);
+        System.out.println(ans);
     }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -659,4 +659,50 @@ class Solution {
         Arrays.sort(nums);
         return IntStream.iterate(0, i -> i < nums.length, i -> i + 2).map(i -> nums[i]).sum();
     }
+
+    /**
+     * 给定一个长度为 n 的整数数组 A 。
+     * <p>
+     * 假设 Bk 是数组 A 顺时针旋转 k 个位置后的数组，我们定义 A 的“旋转函数” F 为：
+     * <p>
+     * F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-1) * Bk[n-1]。
+     * <p>
+     * 计算F(0), F(1), ..., F(n-1)中的最大值。
+     * <p>
+     * 注意:
+     * 可以认为 n 的值小于 105。
+     * <p>
+     * 示例:
+     * <p>
+     * A = [4, 3, 2, 6]
+     * <p>
+     * F(0) = (0 * 4) + (1 * 3) + (2 * 2) + (3 * 6) = 0 + 3 + 4 + 18 = 25
+     * F(1) = (0 * 6) + (1 * 4) + (2 * 3) + (3 * 2) = 0 + 4 + 6 + 6 = 16
+     * F(2) = (0 * 2) + (1 * 6) + (2 * 4) + (3 * 3) = 0 + 6 + 8 + 9 = 23
+     * F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
+     * <p>
+     * 所以 F(0), F(1), F(2), F(3) 中的最大值是 F(3) = 26 。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/rotate-function
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param A
+     * @return
+     */
+    public int maxRotateFunction(int[] A) {
+        int length = A.length;
+        if(length == 0){
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < length; i++) {
+            max = Math.max(rotateFunction(A, i), max);
+        }
+        return max;
+    }
+
+    private int rotateFunction(int[] A, int k) {
+        return IntStream.range(0, A.length).map(i -> i * A[(i + k) % A.length]).sum();
+    }
 }
